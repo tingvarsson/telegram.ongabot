ab00902ae011c5a4f43f50dd17b98175c34cc54a
Thomas Ingvarsson <ingvarsson.thomas@gmail.com>
2026-05-14 10:51:16 +0200

    fix: use fully-qualified import and add menu_description length guard in test_commands

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,6 +1,6 @@
 import unittest
 
-from utils.commands import ALL_COMMANDS, BOT_DESCRIPTION, BOT_SHORT_DESCRIPTION
+from ongabot.utils.commands import ALL_COMMANDS, BOT_DESCRIPTION, BOT_SHORT_DESCRIPTION
 
 
 class CommandInfoMenuDescriptionTest(unittest.TestCase):
@@ -13,6 +13,10 @@ class CommandInfoMenuDescriptionTest(unittest.TestCase):
 
     def test_bot_description_within_telegram_limit(self):
         self.assertLessEqual(len(BOT_DESCRIPTION), 512)
+
+    def test_all_commands_menu_description_within_telegram_limit(self):
+        for cmd in ALL_COMMANDS:
+            self.assertLessEqual(len(cmd.menu_description), 256, f"{cmd.command} menu_description too long")
 
 
 if __name__ == "__main__":
